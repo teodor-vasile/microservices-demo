36b6dc18320e132a04b70435d4caaa89a269fa33
TeodorV <vasileteodor@icloud.com>
2022-11-04 20:44:25 +0100

    Alternative for RestTemplate -> WebClient

--- movie-catalog-service/src/main/java/com/teojava/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/teojava/moviecatalogservice/resources/MovieCatalogResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/catalog")
@@ -19,6 +20,9 @@ public class MovieCatalogResource
 {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
@@ -34,6 +38,15 @@ public class MovieCatalogResource
         return ratings.stream().map(rating ->
                 {
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                    /*This is the equivalent of doing the call with WebClient
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();*/
+
                     return new CatalogItem(movie.getName(), "Cars transforming into robots", rating.getRating());
                 })
                 .collect(Collectors.toList());
